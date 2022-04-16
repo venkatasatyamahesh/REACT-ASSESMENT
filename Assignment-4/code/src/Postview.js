5a8332e9d0e167833a610353a46b7492b8daf01a
venkatasatyamahesh <98881109+venkatasatyamahesh@users.noreply.github.com>
2022-04-16 10:47:38 +0530

    Assignment-4

--- Assignment-4/code/src/Postview.js
+++ Assignment-4/code/src/Postview.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './Postview.css';
+import React ,{ useEffect, useState} from 'react';
 
 
 const Postview=()=> {
+  const[Post,setPost]= useState([]);
+  const getUserData = ()=>{
+    fetch ("http://localhost:3004/user")
+    .then(response=>response.json())
+    .then(res=> setPerson(res))
+    .catch(error=>console.log(error))
+  }
+  useEffect(() =>{
+    getUserData();
+  },[])
   return (
-    <div className="site-container">
-    </div>
+    <>
+     <div className="site-container">
+     </div>
+    <PostTop></PostTop>
+    <Post Postview={Postview}></Post>
+    </>
   );
 }
 
